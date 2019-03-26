d2977246b9da96fc2242919644432d80e30aaefc
Truta446 <juan.versolato@outlook.com>
2019-03-26 12:49:26 -0400

    feat: build input.js robot

---
+++ robots/input.js
@@ -0,0 +1,26 @@
+const readline = require('readline-sync')
+const state = require('./state.js')
+
+function robot() {
+    const content = {
+        maximunSentences: 7,
+        searchTerm: askAndReturnSearchTerm(),
+        prefix: askAndReturnPrefix()
+    }
+
+    state.save(content)
+
+    function askAndReturnSearchTerm() {
+        return readline.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        return selectedPrefixText
+    }
+}
+
+module.exports = robot
