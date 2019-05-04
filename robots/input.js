32a5fb968aad405b012443a3bbe2ffe0160ac362
TRUTA446 <juan.versolato@outlook.com>
2019-05-04 18:32:55 -0400

    feat: Change some things to a project

--- robots/input.js
+++ robots/input.js
@@ -3,7 +3,7 @@ const state = require('./state.js')
 
 function robot() {
     const content = {
-        maximunSentences: 10,
+        maximunSentences: 7,
         searchTerm: askAndReturnSearchTerm(),
         prefix: askAndReturnPrefix()
     }
